2f82e783f62c0ac56e63f785e7920be62f4106f5
Nikhil-Wadatkar-Basket <wadatkarnikhil22@gmail.com>
2023-03-07 10:53:58 +0530

    services created

---
+++ hospital-service/src/test/java/com/bs/hospitalservice/HospitalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.hospitalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
